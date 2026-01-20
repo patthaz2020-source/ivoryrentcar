09aad4c2cb23b756ff6df888572c532a8056bbef
VISHWA PABASARA <patthaz2020@gmail.com>
2026-01-20 14:31:41 +0530

    fixed api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { api } from "../../../api/api";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "https://ivoryrentcar.onrender.com/api",
   withCredentials: true,
 });
 
