093212a10c91c94db3bed93573f0bfc8ee3b0be0
VISHWA PABASARA <patthaz2020@gmail.com>
2026-01-20 11:50:38 +0530

    jkjk

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api", // IMPORTANT for CRA proxy
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5005/api",
   headers: { "Content-Type": "application/json" },
 });
 
